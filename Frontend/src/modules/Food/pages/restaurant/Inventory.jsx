650a0cc4040d5ac0783a8270d51fd80b3716d708
Ruturajsinghchouhan <riturajsinghchouhan007@gmail.com>
2026-04-16 11:55:35 +0530

    zometo fix

--- Frontend/src/modules/Food/pages/restaurant/Inventory.jsx
+++ Frontend/src/modules/Food/pages/restaurant/Inventory.jsx
@@ -2651,7 +2651,7 @@ export default function Inventory() {
                         />
                       </div>
                       <p className="text-sm text-gray-500">
-                        Item won't be visible to customers on Zomato app till you mark it back in stock
+                        Item won't be visible to customers on app till you mark it back in stock
                       </p>
                     </div>
                   </label>
